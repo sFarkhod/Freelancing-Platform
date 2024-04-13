c6c4e4509b7433577cad5b04954fc2b122f4ead7
Doston1995 <ctron1108@gmail.com>
2024-04-13 16:08:52 +0500

    serializer and views

--- user/urls.py
+++ user/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
+from user.views import ClientAPIView, FreelancerAPIView
+
 
 urlpatterns = [
+    path('clients', ClientAPIView.as_view()),
+    path('freelancers', FreelancerAPIView.as_view()),
 ]
