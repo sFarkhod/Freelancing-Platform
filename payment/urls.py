38ab436e878f3733504503d49d814316dc504adc
InomjonQurbonov <inomjonqurbonov916@gmail.com>
2024-04-14 20:09:57 +0500

    Payments Ready

--- payment/urls.py
+++ payment/urls.py
@@ -1,4 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PaymentViewSet, CreditCardViewSet, SubscriptionViewSet, WithdrawViewSet
 
+# Ruterni yaratamiz va uni foydalanuvchilar bilan ro'yxatga olamiz.
+router = DefaultRouter()
+router.register(r'payments', PaymentViewSet)
+router.register(r'credit-cards', CreditCardViewSet)
+router.register(r'subscriptions', SubscriptionViewSet)
+router.register(r'withdraws', WithdrawViewSet)
+
+# API URLlari avtomatik ravishda router tomonidan aniqlanadi.
+# Qo'shimcha ravishda, ko'rinuvchi API uchun kirish URLlarini qo'shib chiqamiz.
 urlpatterns = [
+    path('api/', include(router.urls)),
 ]
--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,77 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Payment, CreditCard, Subscription, Withdraw
+from .serializer import (
+    PaymentSerializer, PaymentDetailSerializer, CreatePaymentSerializer, UpdatePaymentSerializer,
+    DeletePaymentSerializer,
+    CreditCardSerializer, CreditCardDetailSerializer, CreateCreditCardSerializer, UpdateCreditCardSerializer,
+    DeleteCreditCardSerializer,
+    SubscriptionSerializer, SubscriptionDetailSerializer, CreateSubscriptionSerializer, UpdateSubscriptionSerializer,
+    DeleteSubscriptionSerializer,
+    WithdrawSerializer, WithdrawDetailSerializer, CreateWithdrawSerializer, UpdateWithdrawSerializer,
+    DeleteWithdrawSerializer,
+)
 
-# Create your views here.
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PaymentSerializer
+        elif self.action == 'retrieve':
+            return PaymentDetailSerializer
+        elif self.action == 'create':
+            return CreatePaymentSerializer
+        elif self.action == 'update':
+            return UpdatePaymentSerializer
+        elif self.action == 'destroy':
+            return DeletePaymentSerializer
+
+
+class CreditCardViewSet(viewsets.ModelViewSet):
+    queryset = CreditCard.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return CreditCardSerializer
+        elif self.action == 'retrieve':
+            return CreditCardDetailSerializer
+        elif self.action == 'create':
+            return CreateCreditCardSerializer
+        elif self.action == 'update':
+            return UpdateCreditCardSerializer
+        elif self.action == 'destroy':
+            return DeleteCreditCardSerializer
+
+
+class SubscriptionViewSet(viewsets.ModelViewSet):
+    queryset = Subscription.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return SubscriptionSerializer
+        elif self.action == 'retrieve':
+            return SubscriptionDetailSerializer
+        elif self.action == 'create':
+            return CreateSubscriptionSerializer
+        elif self.action == 'update':
+            return UpdateSubscriptionSerializer
+        elif self.action == 'destroy':
+            return DeleteSubscriptionSerializer
+
+
+class WithdrawViewSet(viewsets.ModelViewSet):
+    queryset = Withdraw.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return WithdrawSerializer
+        elif self.action == 'retrieve':
+            return WithdrawDetailSerializer
+        elif self.action == 'create':
+            return CreateWithdrawSerializer
+        elif self.action == 'update':
+            return UpdateWithdrawSerializer
+        elif self.action == 'destroy':
+            return DeleteWithdrawSerializer
