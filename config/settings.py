cfcc45ce6099941020f54fc4fe3171eb0eedbd36
Kamoliddin <108530207+ravshanovdev@users.noreply.github.com>
2024-04-21 19:11:25 +0500

    uzgartirish kiritldi

--- config/settings.py
+++ config/settings.py
@@ -52,31 +52,31 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
-SOCIALACCOUNT_PROVIDERS = {
-
-    'facebook': {
-        'METHOD': 'oauth2',
-        # 'SDK_URL': '//connect.facebook.net/{locale}/sdk.js',
-        'SCOPE': ['email', 'public_profile'],
-        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
-        # 'INIT_PARAMS': {'cookie': True},
-        # 'FIELDS': [
-        #     'id',
-        #     'first_name',
-        #     'last_name',
-        #     'middle_name',
-        #     'name',
-        #     'name_format',
-        #     'picture',
-        #     'short_name'
-        # ],
-        'EXCHANGE_TOKEN': True,
-        # 'LOCALE_FUNC': 'path.to.callable',
-        'VERIFIED_EMAIL': False,
-        # 'VERSION': 'v13.0',
-        'GRAPH_API_URL': 'https://graph.facebook.com/v13.0',
-    }
-}
+# SOCIALACCOUNT_PROVIDERS = {
+#
+#     'facebook': {
+#         'METHOD': 'oauth2',
+#         # 'SDK_URL': '//connect.facebook.net/{locale}/sdk.js',
+#         'SCOPE': ['email', 'public_profile'],
+#         'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+#         # 'INIT_PARAMS': {'cookie': True},
+#         # 'FIELDS': [
+#         #     'id',
+#         #     'first_name',
+#         #     'last_name',
+#         #     'middle_name',
+#         #     'name',
+#         #     'name_format',
+#         #     'picture',
+#         #     'short_name'
+#         # ],
+#         'EXCHANGE_TOKEN': True,
+#         # 'LOCALE_FUNC': 'path.to.callable',
+#         'VERIFIED_EMAIL': False,
+#         # 'VERSION': 'v13.0',
+#         'GRAPH_API_URL': 'https://graph.facebook.com/v13.0',
+#     }
+# }
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
