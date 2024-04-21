99b934b3758a7b330b3bab32fd2f2924ce7aaf03
Kamoliddin <108530207+ravshanovdev@users.noreply.github.com>
2024-04-21 19:08:23 +0500

    uzgartirish kiritldi

--- config/settings.py
+++ config/settings.py
@@ -59,17 +59,17 @@ SOCIALACCOUNT_PROVIDERS = {
         # 'SDK_URL': '//connect.facebook.net/{locale}/sdk.js',
         'SCOPE': ['email', 'public_profile'],
         'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
-        'INIT_PARAMS': {'cookie': True},
-        'FIELDS': [
-            'id',
-            'first_name',
-            'last_name',
-            'middle_name',
-            'name',
-            'name_format',
-            'picture',
-            'short_name'
-        ],
+        # 'INIT_PARAMS': {'cookie': True},
+        # 'FIELDS': [
+        #     'id',
+        #     'first_name',
+        #     'last_name',
+        #     'middle_name',
+        #     'name',
+        #     'name_format',
+        #     'picture',
+        #     'short_name'
+        # ],
         'EXCHANGE_TOKEN': True,
         # 'LOCALE_FUNC': 'path.to.callable',
         'VERIFIED_EMAIL': False,
