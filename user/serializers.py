fcfcf7d0503ed4ff8f9779b5bbae7e558e01e754
InomjonQurbonov <inomjonqurbonov916@gmail.com>
2024-04-24 13:47:17 +0500

    CrediCard auto null is added

--- user/serializers.py
+++ user/serializers.py
@@ -19,7 +19,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     def set_null_value_to_other_model(self, user_instance):
         other_model_instance = CreditCard.objects.create(user=user_instance)
-        other_model_instance.null_field = None
+        other_model_instance.null_field = ('card_holder_name', 'Card_number', 'card_expiration_date')
         other_model_instance.save()
 
 
